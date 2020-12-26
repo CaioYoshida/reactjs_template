ddb42208b4007aea52f332521de6c02b98fbf759
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2020-12-26 12:58:08 -0300

    setting a stable react + typescript version

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import Routes from './routes';
-
-import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
+    <h1>Hello Rockeseat</h1>
   </>
 );
 
